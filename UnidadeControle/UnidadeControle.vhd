a976c2cf429ac228e2bba903e03e0b0e833bc0fe
Arthur Rigo <arthurvrr@hotmail.com>
2025-12-02 08:58:17 -0300

    RDM

--- UnidadeControle/UnidadeControle.vhd
+++ UnidadeControle/UnidadeControle.vhd
@@ -14,7 +14,7 @@
 
 -- PROGRAM		"Quartus II 64-Bit"
 -- VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
--- CREATED		"Fri Nov 28 13:25:35 2025"
+-- CREATED		"Mon Dec 01 22:38:17 2025"
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all; 
